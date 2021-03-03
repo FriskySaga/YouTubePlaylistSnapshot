e86aa47bbbeacfdbb818c8995388c0c451f15a3b
FriskySaga <28899815+FriskySaga@users.noreply.github.com>
2021-03-02 19:43:21 -0500

    Recommend double quotation marks
    By using double quotation marks when configuring the playlist IDs in
    the Python dictionary, this allows the user to use apostrophe
    characters for the name of the playlist

--- MyConfigurationSample.py
+++ MyConfigurationSample.py
@@ -15,8 +15,8 @@ COUNTRY_CODE = 'US'
 # Example: https://www.youtube.com/playlist?list=PLOU2XLYxmsIKpaV8h0AGE05so0fAwwfTw
 # In the example URL, 'PLOU2XLYxmsIKpaV8h0AGE05so0fAwwfTw' is the playlist ID
 PLAYLIST_IDS = {
-  'Your Playlist Name' : 'Your Playlist ID',
-# 'Another Playlist'   : 'Another ID', # Comment out if you don't want to the script to see this playlist
+  "Your Playlist Name" : "Your Playlist ID",
+# "Another Playlist"   : "Another ID", # Comment out if you don't want to the script to see this playlist
 }
 
 # Whether to display the channel name of the video uploader
